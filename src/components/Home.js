42cda9d72e1f741b679b3d354737674cddb5f2c1
Hohenheim <jomtiu16@gmail.com>
2022-08-16 20:38:40 +0800

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import Header from './Header'
 import { AnimeContent } from '../DataProvider'
 import TrendingCarousel from './Carousel/TrendingCarousel'
 import styles from '../styles/throwback.module.scss'
+import AnimeEpisodes from './AnimeEpisodes'
 
 export default function Home() {
 
 	const {AnimeList} = useContext(AnimeContent)
 
 	const throwBackAnime = AnimeList.filter(item => item.status.includes('throwback'))
+
+	const [isIDtb, setIDtb] = useState(null)
 
 	return (
 		<div>
@@ -20,15 +23,19 @@ export default function Home() {
 				</div>
 				<div>
 					<h2>Throwback Anime!</h2>
-					<ul>
-						{
-							throwBackAnime.map((item, i) => (
-								<li key={i}>
-									<img src={item.img} alt="" />
-								</li>
-							))
-						}
-					</ul>
+					{isIDtb === null ?
+						<ul>
+							{
+								throwBackAnime.map((item, i) => (
+									<li onClick={() => (setIDtb(item.id))} key={i}>
+										<img src={item.img} alt="" />
+									</li>
+								))
+							}
+						</ul>
+					:
+						<AnimeEpisodes id={isIDtb} setID={setIDtb}/>
+					}
 				</div>
 			</section>
 		</div>
